9370d267a8cf5cbf5f9883c64a1fc83fc5ad1f32
Бабышев Максим <cmdazaza@gmail.com>
2022-04-06 22:26:44 +0300

    Init

---
+++ app/src/main/java/edu/mirea_ikbo0619/promofinder/MainViewModel.kt
@@ -0,0 +1,8 @@
+package edu.mirea_ikbo0619.promofinder
+
+import androidx.databinding.ObservableBoolean
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    var showToolbar = ObservableBoolean(false)
+}
